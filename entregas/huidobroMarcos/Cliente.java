1b17023e75ab4cccec63ddbc175559c63d497257
Marcos <marcoshuidobrodiegorgmail.com>
2026-04-07 00:22:48 +0200

    crear liga y equipos en cliente

--- entregas/huidobroMarcos/Cliente.java
+++ entregas/huidobroMarcos/Cliente.java
@@ -2,6 +2,27 @@ package entregas.huidobroMarcos;
 
 class Cliente {
     public static void main(String[] args) {
+        
+        Liga liga = new Liga();
+        liga.crearLiga("Liga Universitaria Santander - Temporada 2024/25");
+
+        Equipo losCracks = new Equipo("Los Cracks");
+        losCracks.sumarJugador(8);
+
+        Equipo byteFC = new Equipo("Byte FC");
+        byteFC.sumarJugador(7);
+
+        Equipo compiladores = new Equipo("Compiladores");
+        compiladores.sumarJugador(9);
+
+        liga.añadirEquipo(losCracks);
+        liga.añadirEquipo(byteFC);
+        liga.añadirEquipo(compiladores);
+
+
+
+
+
         crearLiga()
         partidosPendientes()
         reprogramarPartido()
