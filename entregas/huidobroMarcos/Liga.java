13737a93fec0b7902f532169b3628d57702db3de
Marcos <marcoshuidobrodiegorgmail.com>
2026-04-07 00:19:20 +0200

    Atributos y constructor de Liga

--- entregas/huidobroMarcos/Liga.java
+++ entregas/huidobroMarcos/Liga.java
@@ -1,7 +1,33 @@
 package entregas.huidobroMarcos;
 
+import java.util.ArrayList;
+
 class Liga {
-    public Liga liga() {}
+
+    private String nombre;
+    private ArrayList<Equipo> equipos;
+    private ArrayList<Partido> partidos;
+    private Clasificacion clasificacion;
+
+    public Liga liga() {
+        return new Liga();
+    }
+
+    public Liga() {
+        this.equipos = new ArrayList<>();
+        this.partidos = new ArrayList<>();
+        this.clasificacion = new Clasificacion();
+    }
+
+    public void añadirEquipo(Equipo equipo) {
+        this.equipos.add(equipo);
+        this.clasificacion.añadirEquipo(equipo);
+    }
+
+    public void añadirPartido(Partido partido) {
+        this.partidos.add(partido);
+    }
+
     public String Equipos() {}
     public String Clasificacion() {}
     public String Partidos() {}
