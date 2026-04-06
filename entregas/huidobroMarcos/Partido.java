2d453e98de1911f1d11adebf7a69bac9b8bbb002
Marcos <marcoshuidobrodiegorgmail.com>
2026-04-06 22:51:17 +0200

    atributos privados de partido añadidos

--- entregas/huidobroMarcos/Partido.java
+++ entregas/huidobroMarcos/Partido.java
@@ -1,6 +1,14 @@
 package entregas.huidobroMarcos;
 
 class Partido {
+
+    private Equipo local;
+    private Equipo visitante;
+    private String fecha;
+    private int golesLocal;
+    private int golesVisitante;
+    private boolean pendiente;
+
     public String fecha() {}
     public String preguntaEquipos() {}
     public boolean estaPendiente() {}
