8c45255969ac6f23584225494e2fef57819cd9f6
Marcos <marcoshuidobrodiegorgmail.com>
2026-04-07 00:16:15 +0200

    añadidos atributos y constructor de clasificación

--- entregas/huidobroMarcos/Clasificacion.java
+++ entregas/huidobroMarcos/Clasificacion.java
@@ -1,7 +1,24 @@
 package entregas.huidobroMarcos;
 
+import java.util.ArrayList;
+
 class Clasificacion {
-    public Clasificacion clasificacion() {}
+
+    private ArrayList<Equipo> equipos;
+
+    public Clasificacion clasificacion() {
+        return new Clasificacion();
+    }
+
+    public Clasificacion() {
+        this.equipos = new ArrayList<>();
+    }
+
+    public void añadirEquipo(Equipo equipo) {
+        this.equipos.add(equipo);
+    }
+
+    
     public String consultaClasificacion() {}
     public void mostrar() {}
     public String lider() {}
